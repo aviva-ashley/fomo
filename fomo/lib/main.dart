814c90ecab6c52200aa73c3ffe7aed789d5406fc
pgibert3 <paulgibert98@gmail.com>
2019-11-06 15:00:47 -0500

    Added event card categories and horizontal scrolling

--- fomo/lib/main.dart
+++ fomo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,104 +9,115 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class FomoAppBar extends AppBar {
+  FomoAppBar() : super(
+    centerTitle: true,
+    title: Text('FOMO')
+  );
+}
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class FomoEventCard extends Card {
+  FomoEventCard(title) : super(
+    child: Container(
+      width: 175,
+      child: Text(title)
+    )
+  );
+}
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class FomoEventCategory extends Column {
+  FomoEventCategory(String title, List<FomoEventCard> events) : super(
+    children: <Widget> [
+      Padding(child: FomoEventCategoryHeader("Featured"), padding: EdgeInsets.only(top: 10.0)),
+      Padding(child: FomoEventCardRow(events), padding:EdgeInsets.only(top: 10.0))
+    ]
+  );
+}
+
+class FomoEventCategoryHeader extends Row {
+  FomoEventCategoryHeader(title) : super (
+    children: <Widget> [
+      FomoEventCategoryHeaderTitle("Featured"),
+      Expanded(child: Container()),
+      FomoViewAllButton()
+    ],
+    crossAxisAlignment: CrossAxisAlignment.end
+  );
+}
+
+class FomoViewAllButton extends Container {
+  FomoViewAllButton() : super (
+    child: Text("View All >",
+      style: TextStyle(
+        color: Color(0xFF30302f),
+        fontSize: 13.0,
+        fontFamily: 'Raleway Heavy',
+        fontWeight: FontWeight.w500,
+      )
+    )
+  );
+}
+
+class FomoEventCategoryHeaderTitle extends Container {
+  FomoEventCategoryHeaderTitle(title) : super (
+    child: Text(title,
+      style: TextStyle(
+        color: Color(0xFF30302f),
+        fontSize: 20.0,
+        fontFamily: 'Raleway Heavy',
+        fontWeight: FontWeight.w900,
+      )
+    )
+  );
+}
+
+class FomoEventCardRow extends Container {
+  FomoEventCardRow(List<FomoEventCard> events) : super(
+    child : ListView(
+      scrollDirection: Axis.horizontal,
+      children: events
+    ),
+    height: 150.0
+  );
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    List<FomoEventCard> events = [
+      FomoEventCard("Taylor Swift"),
+      FomoEventCard("Elvis Presly"),
+      FomoEventCard("USH"),
+      FomoEventCard("AFTM"),
+      FomoEventCard("Josh Turner")
+    ];
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: FomoAppBar(),
+      body: Padding(padding: EdgeInsets.only(left:20.0, right:20.0, bottom:10.0),
+        child: ListView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            FomoEventCategory("Featured", events),
+            FomoEventCategory("Featured", events),
+            FomoEventCategory("Featured", events),
+            FomoEventCategory("Featured", events),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        )
+      )
     );
   }
 }
